935a6207ed26b60909e514ca8db6b97a30b81733
Jaimeaq11 <jaimeabadq@gmail.com>
2025-12-17 17:35:05 +0100

    primer commit en intellij idea

---
+++ src/main/java/com/jaime/eltumbometro/ElTumbometroApplication.java
@@ -0,0 +1,13 @@
+package com.jaime.eltumbometro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElTumbometroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElTumbometroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jaime/eltumbometro/ElTumbometroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaime.eltumbometro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElTumbometroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
